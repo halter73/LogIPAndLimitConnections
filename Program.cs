7ba9605c95166c0256870af979b8a38342520f22
Stephen Halter <halter73@gmail.com>
2021-08-24 15:04:24 -0700

    Add logging and limiting connection middleware

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +9,10 @@ namespace LogIPAndLimitConnections
 {
     public class Program
     {
+        private const int MaxConnections = 5;
+
+        private static int _connectionCount;
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -21,6 +23,44 @@ namespace LogIPAndLimitConnections
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(kestrelOptions =>
+                    {
+                        kestrelOptions.ListenAnyIP(5000, listenOptions =>
+                        {
+                            var logger = listenOptions.ApplicationServices.GetRequiredService<ILogger<Program>>();
+
+                            listenOptions.Use(next =>
+                            {
+                                return async (connectionContext) =>
+                                {
+                                    var currentConnectionCount = Interlocked.Increment(ref _connectionCount);
+
+                                    try
+                                    {
+                                        if (currentConnectionCount > MaxConnections)
+                                        {
+                                            logger.LogWarning("{ConnectionId} rejected because the concurrent limit exceeded. ({RemoteIP}, {LocalIP})",
+                                                connectionContext.ConnectionId,
+                                                connectionContext.RemoteEndPoint,
+                                                connectionContext.LocalEndPoint);
+                                            return;
+                                        }
+
+                                        logger.LogInformation("{ConnectionId} accepted.",
+                                            connectionContext.ConnectionId,
+                                            connectionContext.RemoteEndPoint,
+                                            connectionContext.LocalEndPoint);
+
+                                        await next(connectionContext);
+                                    }
+                                    finally
+                                    {
+                                        Interlocked.Decrement(ref _connectionCount);
+                                    }
+                                };
+                            });
+                        });
+                    });
                 });
     }
 }
